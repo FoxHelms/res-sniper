d847c6becad87d8781a63577ba0514cafde37b06
Fox Helms <foxhelms@gmail.com>
2023-08-20 21:08:30 -0600

    adding web app tests

--- test_web_app.py
+++ test_web_app.py
@@ -4,10 +4,37 @@ def test_home_page():
     response = app.test_client().get('/')
     assert response.status_code == 200
     assert b'<h1>Reservation Sniper</h1>' in response.data
-    
-'''
+
+
 def test_post_name():
     with app.test_client() as c:
-        response = c.post('/', 'userRest=Mischa')
-        assert response.status_code == 302
-        assert b'<td>Mischa</td>' in response.data'''
+        t_data = {'userRest':'Mischa'}
+        d_post = c.post('/', data=t_data)
+        updated_page = app.test_client().get('/')
+        assert d_post.status_code == 302
+        assert updated_page.status_code == 200
+        assert b'<td>Mischa</td>' in updated_page.data
+
+def test_post_name_should_fail():
+    with app.test_client() as c:
+        t_data = {'userRest':'sushi den'}
+        d_post = c.post('/', data=t_data)
+        assert d_post.status_code == 500
+        assert b'<h1>KeyError</h1>' or b'<h1>Internal Server Error</h1>' in d_post.data
+
+def test_delete():
+    with app.test_client() as c:
+        t_data = {'userRest':'Shukette'}
+        d_post = c.post('/', data=t_data)
+        page_after_added = app.test_client().get('/')
+        deleted_page = app.test_client().get('/delete/1')
+        page_after_deleted = app.test_client().get('/')
+        assert d_post.status_code == 302
+        assert deleted_page.status_code == 302
+        assert page_after_added.status_code == 200
+        assert page_after_deleted.status_code == 200
+        assert b'<td>Shukette</td>' in page_after_added.data
+        assert b'<td>Shukette</td>' not in page_after_deleted.data
+
+
+
