af7cee4462fdbcf7fb812d1cd8a40fce9876ae25
Fox Helms <foxhelms@gmail.com>
2023-08-20 21:30:42 -0600

    testing web app

--- test_web_app.py
+++ test_web_app.py
@@ -22,19 +22,5 @@ def test_post_name_should_fail():
         assert d_post.status_code == 500
         assert b'<h1>KeyError</h1>' or b'<h1>Internal Server Error</h1>' in d_post.data
 
-def test_delete():
-    with app.test_client() as c:
-        t_data = {'userRest':'Shukette'}
-        d_post = c.post('/', data=t_data)
-        page_after_added = app.test_client().get('/')
-        deleted_page = app.test_client().get('/delete/1')
-        page_after_deleted = app.test_client().get('/')
-        assert d_post.status_code == 302
-        assert deleted_page.status_code == 302
-        assert page_after_added.status_code == 200
-        assert page_after_deleted.status_code == 200
-        assert b'<td>Shukette</td>' in page_after_added.data
-        assert b'<td>Shukette</td>' not in page_after_deleted.data
-
 
 
