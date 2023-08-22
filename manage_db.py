78104032a731d85c1e88e2ad9141be9722f773d1
Fox Helms <foxhelms@gmail.com>
2023-08-22 14:00:42 -0600

    query db

--- manage_db.py
+++ manage_db.py
@@ -1,13 +1,14 @@
 import sqlite3
 
 def create_table():
-    conn = sqlite3.connect('restaurant.db')
+    conn = sqlite3.connect('permanent_restaurant.db')
     c = conn.cursor()
-    c.execute('''CREATE TABLE restaurants (
-            restaurant_name text,
-            restaurant_id integer
-            )''')
-    conn.commit()
+    if not c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='all_restaurants'"):
+        c.execute('''CREATE TABLE all_restaurants (
+                restaurant_name text,
+                restaurant_id integer
+                )''')
+        conn.commit()
     conn.close()
 
 
@@ -25,17 +26,18 @@ def write_to_db(restName,id):
     #if entry_in_db(restName):
     #    return
     #else:
-    conn = sqlite3.connect('restaurant.db')
+    conn = sqlite3.connect('permanent_restaurant.db')
     c = conn.cursor()
-    c.execute('INSERT INTO restaurants VALUES (%s,%i)' %(restName,id))
+    # START HERE This is throwing invalid input. 
+    c.execute(f'INSERT INTO all_restaurants VALUES {restName},{id}')
     conn.commit()
     conn.close()
 
 
 def disp_table():
-    conn = sqlite3.connect('restaurant.db')
+    conn = sqlite3.connect('permanent_restaurant.db')
     c = conn.cursor()
-    selectPrompt = 'SELECT * FROM restaurants'
+    selectPrompt = 'SELECT * FROM all_restaurants'
     c.execute(selectPrompt)
     data = c.fetchall()
     for d in data:
@@ -45,7 +47,7 @@ def get_ids():
     ids = []
     conn = sqlite3.connect('restaurant.db')
     c = conn.cursor()
-    selectPrompt = 'SELECT restaurant_id FROM restaurants'
+    selectPrompt = 'SELECT restaurant_id FROM all_restaurants'
     c.execute(selectPrompt)
     data = c.fetchall()
     for d in data:
