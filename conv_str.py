680c91843fcc23b981354af50bae912cb4a72663
Fox Helms <foxhelms@gmail.com>
2023-08-30 18:13:02 -0500

    fixing ignore file

---
+++ conv_str.py
@@ -0,0 +1,11 @@
+from typing import List
+
+'''
+This should be able to take user input and add it to the list that the resbot checks
+'''
+
+def get_rest_from_user(usrStr: str) -> str:
+    '''ask user for restaurant name and convert to url string'''
+    return usrStr.replace(' ','-').lower()
+
+
